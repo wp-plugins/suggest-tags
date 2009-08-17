dd45be8421a6469824c04a9769158104f9151d09
slaptijack <slaptijack@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-17 18:28:19 +0000

    Initial check-in
    git-svn-id: https://plugins.svn.wordpress.org/suggest-tags/trunk@146727 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Suggest Tags ===
+Contributors: slaptijack
+Donate link: http://slaptijack.com/
+Tags: tagging, tag, tags
+Requires at least: 2.7
+Tested up to: 2.8.4
+Stable tag: 1.0.0b1
+
+Suggest Tags uses the tagthe.net tagging web service to generate a list of tag suggestions.
+
+== Description ==
+
+Suggest Tags creates a list of tag suggestions based on the content of the post you are editing. Since the Yahoo! Term Extraction API is going to be shutdown at the end of August 2009, this plugin uses the term extraction API from tagthe.net. 
+
+== Installation ==
+
+1. Download the ZIP file and expand it.
+1. Copy the suggest_tags directory to your wp-content/plugins/ directory.
+1. Activate the plugin via your WordPress Plugins Panel.
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+= 1.0.0b1 =
+* This is the initial beta release of Suggest Tags. Feedback welcome!
+
+== Screenshots ==
---
+++ suggest-tags.php
@@ -0,0 +1,126 @@
+<?php 
+/*
+Plugin Name: Suggest Tags
+Plugin URI: http://slaptijack.com/projects/
+Description: suggest tags
+Version: 1.0.0b1
+Author: Scott Hebert
+Author URI: http://www.heberts.net/
+*/
+
+class SuggestTags {
+  function add_tag_box() {
+    if ( function_exists('add_meta_box') ) {
+      add_meta_box('tag_suggestions', 'Tag Suggestions', array('SuggestTags', 'add_new_box'), 'post', 'side', 'low');
+    } else {
+      add_action('dbx_post_advanced', array('SuggestTags', 'add_old_box'));
+    }
+  }
+  
+  function add_new_box() {
+    ?>
+<p>
+  <input name="get_suggestions" type="button" onclick="suggest_tags_get_suggestions()" value="<?php _e('Suggest Tags') ?>">
+  <div id="suggestion_results"></div>
+</p>
+    <?php
+    
+  }
+
+	function add_old_box() {
+		?>
+<fieldset id="tagsuggestions" class="dbx-box">
+<h3 class="dbx-handle"><?php _e('Tag Suggestions') ?></h3>
+<div class="dbx-content">
+	<p>
+		<input name="get_suggestions" type="button" onclick="suggest_tags_get_suggestions()" value="<?php _e('Get Tag Suggestions') ?>">
+		<div id="suggestion_results"></div>
+	</p>
+</div>
+</fieldset>
+<?
+	}
+
+	function add_javascript() {
+	  // use JavaScript SACK library for AJAX
+	  wp_print_scripts(array( 'sack' ));
+
+	  // Define custom JavaScript function
+	?>
+<script type="text/javascript">
+	//<![CDATA[
+	
+	function suggest_tags_get_suggestions() {
+	  var mysack = new sack("<?php bloginfo( 'wpurl' ); ?>/wp-admin/admin-ajax.php");    
+
+	  mysack.execute = 1;
+	  mysack.method = 'POST';
+	  mysack.setVar( "action", "suggest_things_ajax_get_suggestions" );
+	  mysack.setVar( "content", document.getElementById("content").value);
+	  mysack.encVar( "cookie", document.cookie, false );
+	  mysack.onError = function() { alert('AJAX error while looking up tag suggestions' ) };
+	  mysack.runAJAX();
+		
+	  return TRUE;
+	}
+	
+	function process_matches(matches) {
+		html = "";
+		htmlall = "";
+		if(matches.memes[0]) {
+			for(i = 0; i < matches.memes[0].dimensions.topic.length; i++) {
+				html += "<a onclick=\"suggest_tags_add_tag('" + matches.memes[0].dimensions.topic[i] +"')\">" + matches.memes[0].dimensions.topic[i] +"</a><br />";
+				if (htmlall != "") htmlall += ", ";
+				htmlall += matches.memes[0].dimensions.topic[i];
+			}
+
+			html += "<br /><a onclick=\"suggest_tags_add_tag('" + htmlall + "')\"><?php _e('Add All') ?></a>";
+		} else {
+			html = "<i><?php _e('No Suggestions') ?></i>";
+		}
+		elt = document.getElementById("suggestion_results");
+		elt.innerHTML = html;
+	}
+	
+	function suggest_tags_add_tag(tag) {
+	  tag_field = document.getElementById('new-tag-post_tag');
+		if (tag_field.value == "" || tag_field.value == "Add new tag" ) {
+			tag_field.value = tag;
+		} else {
+			tag_field.value += ", " + tag;
+		}
+	}
+	//]]>
+</script>
+<?php
+	}
+
+	function get_suggestions() {
+		$content = $_POST['content'];
+		$title   = $_POST['title'];
+		$content = stripslashes($content) . ' ' . stripslashes($title);
+		
+		if ( empty($content) ) {
+		  error_log("No text received.");
+		  exit();
+		}
+		
+		$r = wp_remote_post('http://tagthe.net/api/?text='.urlencode($content).'&view=json&count=20');
+		
+		if ( !is_wp_error($r) ) {
+		  $code = wp_remote_retrieve_response_code($r);
+		  if ($code == 200 ) {
+		    $json = maybe_unserialize(wp_remote_retrieve_body($r));
+		  } else {
+		    error_log("Received error code from tagthe.net: $code"); 
+		  }
+		}
+		
+		die("process_matches(eval(" . $json . "));");
+	}
+}
+
+add_action('admin_print_scripts', array('SuggestTags','add_javascript'));
+add_action('admin_menu', array('SuggestTags', 'add_tag_box'));
+add_action('wp_ajax_suggest_things_ajax_get_suggestions', array('SuggestTags','get_suggestions'));
+?>
